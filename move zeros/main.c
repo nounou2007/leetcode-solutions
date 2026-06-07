6da6b42a08e52c14adfea6c863e967f13c0eb9fe
hanine kynda <kyndahanineboumaza@gmail.com>
2026-06-08 00:23:30 +0200

    move zeroes

--- move zeros/main.c
+++ move zeros/main.c
@@ -1,16 +1,18 @@
 void moveZeroes(int *nums, int numsSize)
 {
-    int last = 0;
-    for (int i = 0; i < numsSize; i++)
+    int last = 0;                      // tracks next available position for non-zero
+    for (int i = 0; i < numsSize; i++) // runs n times
     {
-        if (nums[i] != 0)
+        if (nums[i] != 0) // found a non-zero element
         {
-            nums[last] = nums[i];
-            last++;
+            nums[last] = nums[i]; // place it at next available spot
+            last++;               // move available spot forward
         }
     }
-    for (int j = last; j < numsSize; j++)
+    for (int j = last; j < numsSize; j++) // runs remaining positions
     {
-        nums[j] = 0;
+        nums[j] = 0; // fill rest with zeros
     }
 }
+// time complexity = O(n)
+// space complexity = O(1)
