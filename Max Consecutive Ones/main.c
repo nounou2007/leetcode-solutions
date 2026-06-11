56572e68368c8e1772dc1cb2604b6d4fa8642938
hanine kynda <kyndahanineboumaza@gmail.com>
2026-06-11 16:37:21 +0200

    Max Consecutive Ones

--- Max Consecutive Ones/main.c
+++ Max Consecutive Ones/main.c
@@ -1,3 +1,24 @@
-int findMaxLength(int* nums, int numsSize) {
-   
+int findMaxConsecutiveOnes(int *nums, int numsSize)
+{
+    int sum = 0;
+    int max_sum = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        int temp = sum;
+        if (nums[i] == 1)
+        {
+            sum++;
+            if (sum > max_sum)
+                max_sum = sum;
+        }
+        else
+        {
+            if (temp > max_sum)
+            {
+                max_sum = temp;
+            }
+            sum = 0;
+        }
+    }
+    return max_sum;
 }
