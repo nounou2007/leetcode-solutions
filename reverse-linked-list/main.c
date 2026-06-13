46dd7faff49e25fe66f5adf58214b7ab19ebee9c
hanine kynda <kyndahanineboumaza@gmail.com>
2026-06-13 22:29:50 +0200

    reverse-linked-list

---
+++ reverse-linked-list/main.c
@@ -0,0 +1,21 @@
+// this is not in leet code
+// typedef struct ListNode
+//{
+//    int val;
+//    struct ListNode *next;
+// } ListNode;
+struct ListNode *reverseList(struct ListNode *head)
+{
+    ListNode *prev = NULL;
+    ListNode *cur = head;
+    ListNode *next = NULL;
+
+    while (cur != NULL)
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    return prev;
+}
