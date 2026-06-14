eac3058e90f1517d4e7ff24917d9deac9c570e05
hanine kynda <kyndahanineboumaza@gmail.com>
2026-06-14 17:07:49 +0200

    find-minimum-in-rotated-sorted-array

---
+++ find-minimum-in-rotated-sorted-array/main.c
@@ -0,0 +1,15 @@
+
+int findMin(int* nums, int numsSize) {
+    int left=0;
+    int right=numsSize-1;
+    
+    while(left<right){
+        int mid=(left+right)/2;
+        if(nums[mid] > nums[right]){
+            right=mid;
+        }else{
+            left=mid+1;
+        }
+    }
+return nums[left];
+}
