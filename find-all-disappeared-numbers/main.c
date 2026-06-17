5ef24704152064663ef915f1704b1c0334785e5a
hanine kynda <kyndahanineboumaza@gmail.com>
2026-06-17 17:36:02 +0200

    find-all-disappeared-numbers

--- find-all-disappeared-numbers/main.c
+++ find-all-disappeared-numbers/main.c
@@ -1,32 +1,27 @@
 #include <stdlib.h>
-int compare(const void *a, const void *b)
-{
-    return *(int *)a - *(int *)b;
-}
-int *findDisappearedNumbers(int *nums, int numsSize, int *returnSize)
-{
-    qsort(nums, numsSize, sizeof(int), compare);
-    int *result = malloc(numsSize * sizeof(int));
-    *returnSize = numsSize;
-    int i = 0;
-    int j = 0;
-    while (i < numsSize)
-    {
-        int seen[numsSize] = {0};
-        for (int i = 0; i < numsSize; i++)
-            seen[nums[i] - 1] = 1;
 
-        for (int i = 0; i < numsSize; i++)
-            if (seen[i] == 0)
-                result[j++] = i + 1;
-        if (nums[i] != i + 1)
-        {
-            result[j] = i + 1;
-            j++;
-        }
-        else
-        {
-            i++;
-        }
+int compare(const void *a, const void *b) {
+    return (*(int*)a > *(int*)b) - (*(int*)a < *(int*)b);
+}
+
+int *findDisappearedNumbers(int *nums, int numsSize, int *returnSize) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int *result = malloc(sizeof(int) * numsSize);
+    int i = 0, j = 1, count = 0;
+
+   while (j <= numsSize) {
+    if (i < numsSize && nums[i] == j) {
+        i++;
+        j++;
+    } else if (i < numsSize && nums[i] > j) {
+        result[count++] = j++;
+    } else if (i < numsSize && nums[i] == nums[i-1]) {
+        i++;
+    } else {
+        result[count++] = j++;
     }
+}
+    *returnSize = count;
+    return result;
 }
