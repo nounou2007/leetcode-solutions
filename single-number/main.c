f33bc6e525acb14e429574c93bdeb472d04e9007
hanine kynda <kyndahanineboumaza@gmail.com>
2026-06-19 23:08:48 +0200

    single-number

--- single-number/main.c
+++ single-number/main.c
@@ -19,3 +19,14 @@ int singleNumber(int *nums, int numsSize)
     }
     return -1;
 }
+
+// the bit wise using xor solution
+int singleNumber(int *nums, int numsSize)
+{
+    int result = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        result = result ^ nums[i];
+    }
+    return result;
+}
